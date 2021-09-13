6bda2817322b6581b641ae375d05e23438ba46f5
Ryan-Sandhoefner <Mngamer813@gmail.com>
2021-09-13 09:40:00 -0500

    5 favorite movies

---
+++ main.py
@@ -0,0 +1,14 @@
+#print first movie
+print("lion king")
+
+#print second movie
+print("finding nemo")
+
+#print third movie
+print("lord of the rings")
+
+#print fourth movie
+print("happy gilmore")
+
+#print fith movie
+print("telledega nights")
