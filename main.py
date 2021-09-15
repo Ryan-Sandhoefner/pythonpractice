11f5016a67dfa874b7e59e946a8cf97ebfd07103
Ryan-Sandhoefner <Mngamer813@gmail.com>
2021-09-15 09:14:23 -0500

    add top five videogames, sports, and three hobbies

--- main.py
+++ main.py
@@ -14,5 +14,53 @@ def topFiveMovies():
      #print fifth movie
     print("bee movie")
 
+#define function
+def topFiveSports():
+      #print first sport
+     print("American Football")
+
+    #print second sport
+     print("Soccer")
+
+     #print third sport
+     print("Golf")
+
+    #print fourth sport
+     print("Basketball")
+
+     #print fifth sport
+     print("Mountain biking")
+
+#define function
+def topFiveVideoGames():
+      #print first Videogame
+     print("Minecraft")
+
+    #print second videogame
+     print("Skyrim")
+
+     #print third videogame
+     print("Dragon Age: Inquisition")
+
+    #print fourth Videogame
+     print("Super Mario Bros")
+
+     #print fifth Videogame
+     print("Terraria")
+
+#define function
+def topFiveHobbies():
+      #print first Hobbie
+     print("biking")
+
+    #print second Hobbie
+     print("Hiking")
+
+     #print third Hobbie
+     print("Playing games")
+
+
 print("These Are My Top Five Movies")
 topFiveMovies()
+print("These are my Top Five Sports")
+topFiveSports()
