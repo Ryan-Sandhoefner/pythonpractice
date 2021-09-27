1913f35359ef5b7a5479140605ecfed3e51fd681
Ryan-Sandhoefner <Mngamer813@gmail.com>
2021-09-27 09:34:36 -0500

    more if else statement practice

--- main.py
+++ main.py
@@ -1,9 +1,56 @@
-def greaterThan10(x):
-    if x > 10:
-        return "x is greater than 10."
-    elif x == 10:
-        return "x equals 10"
+def equalsTen(x):
+    if x == 10:
+        return "x equals 10!!!!!!!"
+
+    elif x > 10:
+        return "x is greater than 10"
+
     else:
-        return "x is not greater than 10"
-    
-print(greaterThan10(5))
+        return "x is not equal to 10"
+
+print(equalsTen(10))
+
+def favColor(color):
+    if color == "red":
+        return "your favorite color is red"
+
+    else:
+        return "your favorite color is not red"
+
+print(favColor("red"))
+
+def define(word):
+    if word == "hey":
+        return "(used as an exclamation to call attention or to express pleasure, surprise, bewilderment, etc.)"
+
+    elif word == "horse":
+        return "a large, solid-hoofed, herbivorous quadruped, Equus caballus, domesticated since prehistoric times, bred in a number of varieties, and used for carrying or pulling loads, for riding, and for racing."
+
+    elif word == "turtle":
+        return "any reptile of the order Testudines, comprising aquatic and terrestrial species having the trunk enclosed in a shell consisting of a dorsal carapace and a ventral plastron."
+
+    elif word == "mammal":
+        return "any vertebrate of the class Mammalia, having the body more or less covered with hair, nourishing the young with milk from the mammary glands, and, with the exception of the egg-laying monotremes, giving birth to live young."
+
+    elif word == "dictionary":
+        return "a book giving information on particular subjects or on a particular class of words, names, or facts, usually arranged alphabetically:"
+
+    elif word == "synonym":
+        return "a word having the same or nearly the same meaning as another in the language, as happy, joyful, elated. A dictionary of synonyms and antonyms (or opposites), such as Thesaurus.com, is called a thesaurus."
+
+    elif word == "leg":
+        return "either of the two lower limbs of a biped, as a human being, or any of the paired limbs of an animal, arthropod, etc., that support and move the body."
+
+    elif word == "massive":
+        return "large in scale, amount, or degree"
+
+    elif word == "cow":
+        return "the mature female of a bovine animal, especially of the genus Bos."
+
+    elif word == "capable":
+        return "having power and ability; efficient; competent"
+
+    else:
+        return "I dont know"
+
+print(define("leg"))
