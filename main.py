a225a6761a5e478cc7a5654d3c3fbfea7debb47b
Ryan-Sandhoefner <Mngamer813@gmail.com>
2021-09-30 08:53:10 -0500

    1

--- main.py
+++ main.py
@@ -20,37 +20,39 @@ def favColor(color):
 print(favColor("red"))
 
 def define(word):
-    if word == "hey":
+    if word.lower() == "hey":
         return "(used as an exclamation to call attention or to express pleasure, surprise, bewilderment, etc.)"
 
-    elif word == "horse":
+    elif word.lower() == "horse":
         return "a large, solid-hoofed, herbivorous quadruped, Equus caballus, domesticated since prehistoric times, bred in a number of varieties, and used for carrying or pulling loads, for riding, and for racing."
 
-    elif word == "turtle":
+    elif word.lower() == "turtle":
         return "any reptile of the order Testudines, comprising aquatic and terrestrial species having the trunk enclosed in a shell consisting of a dorsal carapace and a ventral plastron."
 
-    elif word == "mammal":
+    elif word.lower() == "mammal":
         return "any vertebrate of the class Mammalia, having the body more or less covered with hair, nourishing the young with milk from the mammary glands, and, with the exception of the egg-laying monotremes, giving birth to live young."
 
-    elif word == "dictionary":
+    elif word.lower() == "dictionary":
         return "a book giving information on particular subjects or on a particular class of words, names, or facts, usually arranged alphabetically:"
 
-    elif word == "synonym":
+    elif word.lower() == "synonym":
         return "a word having the same or nearly the same meaning as another in the language, as happy, joyful, elated. A dictionary of synonyms and antonyms (or opposites), such as Thesaurus.com, is called a thesaurus."
 
-    elif word == "leg":
+    elif word.lower() == "leg":
         return "either of the two lower limbs of a biped, as a human being, or any of the paired limbs of an animal, arthropod, etc., that support and move the body."
 
-    elif word == "massive":
+    elif word.lower() == "massive":
         return "large in scale, amount, or degree"
 
-    elif word == "cow":
+    elif word.lower() == "cow":
         return "the mature female of a bovine animal, especially of the genus Bos."
 
-    elif word == "capable":
+    elif word.lower() == "capable":
         return "having power and ability; efficient; competent"
 
     else:
         return "I dont know"
 
 print(define("leg"))
+
+print(99 % 100) 
