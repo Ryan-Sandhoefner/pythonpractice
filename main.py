40b24bde5f04843a21e6aab1ba796c527e291378
Ryan-Sandhoefner <Mngamer813@gmail.com>
2021-10-01 08:43:04 -0500

    w

---
+++ main.py
@@ -0,0 +1,12 @@
+def function():
+    count = 1
+    potato = 1
+
+    while potato <= 10:
+        while count <= 10:
+            print(count)
+            count = count + 1
+        potato = potato + 1
+        count = 1
+
+function()
